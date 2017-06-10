c402765bf18d1ad1e8ff813a7512a63acef6874b
aridai <aridai.net@outlook.jp>
2017-06-10 10:57:33 +0900

    ResetAfterを追加した。

---
+++ TelekinesisMod/src/Rx/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+
+namespace TelekinesisMod
+{
+    public static class Extensions
+    {
+        public static T AddTo<T>(this T source, ObservableScript script) where T : IDisposable
+        {
+            script?.CompositeDisposable.Add(source);
+            return source;
+        }
+
+        public static UniRx.IObservable<TSource> ResetAfter<TSource>(this UniRx.IObservable<TSource> source, TSource defaultValue, TimeSpan dueTime, IScheduler scheduler)
+        {
+            return new ResetAfterObservable<TSource>(source, defaultValue, dueTime, scheduler);
+        }
+
+        public static UniRx.IObservable<TSource> ResetAfter<TSource>(this UniRx.IObservable<TSource> source, TSource defaultValue, TimeSpan dueTime)
+        {
+            return source.ResetAfter(defaultValue, dueTime, Scheduler.DefaultSchedulers.TimeBasedOperations);
+        }
+
+        public static UniRx.IObservable<TSource> ResetAfter<TSource>(this UniRx.IObservable<TSource> source, TimeSpan dueTime)
+        {
+            return source.ResetAfter(default(TSource), dueTime, Scheduler.DefaultSchedulers.TimeBasedOperations);
+        }
+    }
+}
---
+++ TelekinesisMod/src/Telekinesis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using UniRx;
+
+namespace TelekinesisMod
+{
+    public class Telekinesis : ObservableScript
+    {
+        public const float MaxDistance = 100.0f;
+
+        public ReactiveProperty<Entity> TargetCandidate { get; }
+
+        public Telekinesis()
+        {
+            TargetCandidate =
+                TickAsObservable
+                .Where(_ => Game.Player.Character.Weapons.Current.Hash == WeaponHash.Unarmed)
+                .Select(_ => GetTargetCandidate())
+                .Where(c => c != null)
+                .ResetAfter(null, TimeSpan.FromSeconds(2))
+                .ObserveOn(Scheduler)
+                .ToReactiveProperty()
+                .AddTo(this);
+
+            TargetCandidate.Subscribe(t => UI.ShowSubtitle($"{(t == null ? "null" : "non-null")}")).AddTo(this);
+        }
+
+        //  ターゲット候補を取得する
+        private Entity GetTargetCandidate()
+        {
+            var entity = World.RaycastCapsule(GameplayCamera.Position, GameplayCamera.Direction, MaxDistance, 2.0f, IntersectOptions.Everything, Game.Player.Character).HitEntity;
+            return entity is Ped || entity is Vehicle ? entity : null;
+        }
+    }
+}
